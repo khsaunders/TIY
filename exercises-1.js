38921100af9271ac9ef5cf438a865fca00142896
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-05-23 23:39:22 -0400

    Add definitons

--- exercises-1.js
+++ exercises-1.js
@@ -8,11 +8,44 @@
 
 // .sort()
 
+var names = ['Finn', 'Rey', 'BB-8', 'Poe']
+
+names.sort()
+["BB-8", "Finn", "Poe", "Rey"]
+
+//It organizes whatever is in the array in alphabetical order!
+
 // .concat()
+
+var westeros = ['Jon', 'Sansa', 'Brienne', 'Hodor'],
+    hogwarts = ['Hermione','Ron','Harry','Hagrid'];
+
+  var westerosHogwarts = westeros.concat(hogwarts);
+
+  console.log(bookFandom);
+  [ 'Jon', 'Sansa', 'Brienne', 'Hodor', 'Hermione', 'Ron', 'Harry', 'Hagrid' ]
+
+//It combines the contents of both arrays
 
 // .indexOf()
 
+var names = ['Finn', 'Rey', 'BB-8', 'Poe']
+
+array.indexOf('BB-8'); //0
+array.indexOf('Rey'); //1
+
+//It can find either the position of an item in an array OR help find the occurrences of an item
+
 // .split()
+
+var hodor = 'Hold the door';
+
+var splits = hodor.split(' ', 2);
+
+console.log(splits); // ['Hold''the']
+
+//This helps arrange strings into arrays (???)
+
 
 // .join()
 
