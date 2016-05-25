9a7c66f12a8bf711486d1c999d25c8693d1b42b8
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-05-25 13:16:28 -0400

    Add initial contnet

---
+++ DOMFather/DOMFather.js
@@ -0,0 +1,4 @@
+getElementById = 'fullname' innerHTML = 'Kaitlin Saunders'
+getElementById = 'hometown' innerHTML = 'Sammamish, WA'
+getElementById = 'movie' innerHTML = 'Mad Max: Fury Road'
+getElementById = 'food' innerHTML = 'pizzaaaa'
