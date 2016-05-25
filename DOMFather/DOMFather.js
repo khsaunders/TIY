2e4a126c00556297b83a324c7e42c21c31f61d19
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-05-25 15:31:29 -0400

    Add visible span text and sans-serif font

--- DOMFather/DOMFather.js
+++ DOMFather/DOMFather.js
@@ -1,6 +1,12 @@
 // var myHeading = document.querySelector = ('h1').style.color= #E21E1E;
-// myHeading.textContent= 'About Me'
+// myHeading.innerHTML= 'About Me'
 
+//Using JavaScript, change the body's tag style so it has a font-family of the sans-serif font stack of your choice.
+document.querySelector("body").style.fontFamily = "sans-serif";
+
+document.querySelector("li").style.color = #E21E1E;
+
+//replace spans with own information
 var fullName = document.getElementById('fullname');
 fullName.innerHTML = 'Kaitlin Saunders';
 
@@ -12,3 +18,6 @@ movie.innerHTML = 'Mad Max: Fury Road';
 
 var food = document.getElementById ('food');
 food.innerHTML = 'pizzaaaa';
+
+//Put each goal in JS to keep track of stuff
+//change class then make it color red.
