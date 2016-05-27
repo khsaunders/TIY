1f8da819d3c392e7073cc8f07a5cdbe5eacc0189
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-05-27 14:00:27 -0400

    Finally piecing together elements from js. Commented out a few unnecessary things from HTML

---
+++ nested-accordion/accordion.js
@@ -0,0 +1,13 @@
+/* First attempt
+var = "button"
+ function revealList ();
+ if { 'click'
+  }
+console.log(buttonClick);
+
+ document.getElementByClass("button").addEventListener('click', function ("revealList")) */
+
+var list = document.getElementByClass("revealList");
+var i
+
+for
