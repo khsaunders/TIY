50770b3fc10e4bfda5874bcfb65c71c04c3714d9
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-01 00:55:22 -0400

    Add three attempts box generating in JS

---
+++ modals/dynamic-list/dynamic-list.js
@@ -0,0 +1,33 @@
+$(function(){
+  'use strict';
+});
+
+//Attempt number 1
+// $( "textarea" ).click(function() {
+//   $( ".newitem" ).focus();
+// });
+
+
+//Attempt number 2
+// $('textarea').keyup('click', function(){
+//   // $(this).text('Sign up, fool!');
+//   $('.new-item, textarea').addClass('showing');
+// });
+//
+//
+//
+// $('body').on('click', function(){
+//   $('.new-item, textarea').removeClass('showing');
+// });
+
+var nextBox =
+
+$("textarea").keyup(function(){
+  $("textarea").on("string", true);
+    $(".newbox").addClass('showing');//inspector is telling me that 
+
+  console.log(nextBox);
+
+var hi = 'hello!'
+console.log(hi);
+});
