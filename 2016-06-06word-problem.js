3dd4fe3c3450eb39d16c1057bc235c60ed50dcb5
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-06 10:35:12 -0400

    Word problem today

---
+++ 2016-06-06word-problem.js
@@ -0,0 +1,20 @@
+array holds top choices
+string + position of thing
+
+
+console.log ("My favorite food is" + [i])
+
+
+
+var foodArray = ["pizza", "Jimmy Johns", "beer", "sushi", "cereal"]
+
+// var len = foodArray.length
+
+for (i = 0; i < foodArray.length; i++);
+console.log ("My first favorite food is " + foodArray [0]);
+console.log ("My second favorite food is " + foodArray [1]);
+console.log("My first favorite food is " + foodArray [2]);
+
+
+//  if (i in foodArray) {
+// console.log("My first favorite food is " +  foodArray[i])}
