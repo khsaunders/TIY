ac09597737eea2af623ea94255fe07bcefffc411
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-10-08 22:02:17 -0400

    Add redesign and functionality

--- modals/reg-modal/reg-modal.js
+++ modals/reg-modal/reg-modal.js
@@ -2,18 +2,6 @@ $(function(){
   'use strict';
 });
 
-//Attempt Number 1
-// var a = 1
-// console.log(a);
-//
-// $(".clickme").click (function(){
-//   alert(".modal-container.showing" .click())
-//   alert(".modal.showing" .click())
-//   alert(".modal-container.showing" .click())
-// });
-
-//Attempt Number 2, after some Googling and note review
-
 $('.clickme').on('click', function(){
   $(this).text('Look forward to hearing from you');//changes text in button
   $('.modal-container, .modal').addClass('showing');
@@ -21,7 +9,7 @@ $('.clickme').on('click', function(){
 
 
 
-$('.fa').on('click', function(){
+$('.fa-times, #submitEmail').on('click', function(){
   $('.modal-container, .modal').removeClass('showing');
   console.log('clicked');
 });
